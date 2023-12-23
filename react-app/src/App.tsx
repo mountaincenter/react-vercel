015b9d284b6f0fb43feb7962d2ddcaaff0b8ee38
mountaincenter <y.hiroyuki560730@gmail.com>
2023-12-23 23:54:01 +0900

    create zxing

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
-import Camera from './components/Camera';
+import { useState } from 'react';
+import { Scanner } from './components/Scanner';
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 
-const App: React.FC = () => {
+export const App = () => {
+  const [codes, setCodes] = useState<string[]>([]);
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
   return (
-    <>
-      <Camera />
-    </>
+    <Container>
+      {matches ? (
+        <Box display={'flex'} gap={2}>
+          <Box flex={1}>
+            <Scanner
+              onReadCode={(result) =>
+                setCodes((codes) =>
+                  Array.from(new Set([...codes, result.getText()]))
+                )
+              }
+            />
+          </Box>
+          <Stack width={300} spacing={2}>
+            <TextField fullWidth multiline rows={10} value={codes.join('\n')} />
+            <Button variant={'contained'} fullWidth>
+              コピー
+            </Button>
+          </Stack>
+        </Box>
+      ) : (
+        <Stack spacing={2}>
+          <Scanner
+            onReadCode={(result) =>
+              setCodes((codes) =>
+                Array.from(new Set([...codes, result.getText()]))
+              )
+            }
+          />
+          <TextField fullWidth multiline rows={5} value={codes.join('\n')} />
+          <Button variant={'contained'} fullWidth>
+            コピー
+          </Button>
+        </Stack>
+      )}
+    </Container>
   );
 };
-
-export default App;
---
+++ react-app/src/components/Scanner.tsx
@@ -0,0 +1,31 @@
+import { BrowserMultiFormatReader } from '@zxing/browser';
+import { Result } from '@zxing/library';
+import { useMemo, useRef } from 'react';
+import { useDebounce } from 'react-use';
+
+type ScannerProps = {
+  onReadCode?: (text: Result) => void;
+};
+
+export const Scanner = ({ onReadCode }: ScannerProps) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const codeReader = useMemo(() => new BrowserMultiFormatReader(), []);
+
+  useDebounce(async () => {
+    if (!videoRef.current) return;
+    await codeReader.decodeFromVideoDevice(
+      undefined,
+      videoRef.current,
+      (result, error) => {
+        if (!result) return;
+        if (error) {
+          console.log('ERROR!! : ', error);
+          return;
+        }
+        onReadCode?.(result);
+      }
+    );
+  }, 2000);
+
+  return <video style={{ width: '100%' }} ref={videoRef} />;
+};
